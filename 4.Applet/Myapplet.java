dec3ac997d2ca67c4f2461b50be85ec1b0c42a05
Rajan Sah <76938099+rajan672@users.noreply.github.com>
2022-08-05 09:26:20 +0530

    Applet

---
+++ 4.Applet/Myapplet.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+class Myapplet extends Applet{
+	public void paint (Graphics g){
+		g.drawString("Rajan",100,100);
+	}
+}
+/* <APPLET code = "Myapplet.class" height = "150" width = "200"> */
